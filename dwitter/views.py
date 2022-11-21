f9ccb7450b5e220a0a71506da76966b3a846eda8
Hunter Chambers <chambersh1129@gmail.com>
2022-11-21 17:25:05 -0500

    added follow/unfollow buttons to profile detail view; made it so users cannot follow/unfollow themselves; made it so profile is not listed in follows/followed by list

--- dwitter/views.py
+++ dwitter/views.py
@@ -12,12 +12,32 @@ class DashboardView(TemplateView):
 
 
 class ProfileDetailView(DetailView):
+    # DetailView will automagically look for dwitter/templates/dwitter/<model>_detail.html for template
+    # username must be unique so we can safely change the slug_field and slug_url_kwarg
+    # instead of /profile/1 we can now use /profile/<username>
     model: Type[Model] = Profile
     slug_field: str = "user__username"
     slug_url_kwarg: str = "username"
 
+    def post(self, request, *args, **kwargs):
+        # Users should not be able to follow/unfollow themselves so make sure
+        self.object = self.get_object()
+        current_user_profile = request.user.profile
+
+        if self.object != current_user_profile:
+            action = request.POST.get("follow")
+            if action == "follow":
+                current_user_profile.follows.add(self.object)
+            elif action == "unfollow":
+                current_user_profile.follows.remove(self.object)
+            current_user_profile.save()
+
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
+
 
 class ProfileListView(ListView):
+    # ListView will automagically look for dwitter/templates/dwitter/<model>_list.html for template
     model: Optional[Type[Model]] = Profile
 
     def get_queryset(self) -> QuerySet[Profile]:
