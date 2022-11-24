cde2d501f96d1c75d24a672af494e7af02396187
Hunter Chambers <chambersh1129@gmail.com>
2022-11-24 06:24:15 -0500

    major refactor of the templates and views

--- dwitter/views.py
+++ dwitter/views.py
@@ -3,25 +3,43 @@ from typing import Any, Dict, Optional, Type
 from django.core.paginator import Paginator
 from django.db.models import Model, QuerySet
 from django.forms import BaseForm, BaseModelForm
-from django.http import HttpRequest, HttpResponse, HttpResponseForbidden, HttpResponseRedirect
+from django.http import HttpRequest, HttpResponseForbidden, HttpResponseRedirect
 from django.urls import reverse
 from django.views import View
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import DetailView, ListView
 from django.views.generic.detail import SingleObjectMixin
+from django.views.generic.edit import FormMixin, ProcessFormView
 
 from .forms import DweetForm
 from .models import Dweet, Profile
 
 
-class DweetFormMixin:
+class DweetFormMixin(FormMixin):
     """
     Any view that renders templates can add this mixin to add the DweetForm to the context
     """
 
+    def form_valid(self, form: BaseModelForm) -> HttpResponseRedirect:
+        form.instance.user = self.request.user
+        form.save()
+        return super().form_valid(form)
+
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
-        context: Dict[str, Any] = super().get_context_data(**kwargs)
-        context["dweet_form"] = DweetForm
+        context: Dict[str, Any] = super(DweetFormMixin, self).get_context_data(**kwargs)  # type: ignore
+        if "dweet_form" not in context:
+            context["dweet_form"] = DweetForm
+        if "display_dweet_form" not in context:
+            context["display_dweet_form"] = True
         return context
+
+    def get_form_class(self) -> Type[BaseForm]:
+        return DweetForm
+
+    def get_success_url(self):
+        """
+        Redirect the user to the page they submitted from
+        """
+        return self.request.META.get("HTTP_REFERER")
 
 
 class DashboardView(DweetFormMixin, ListView):
@@ -37,19 +55,20 @@ class DashboardView(DweetFormMixin, ListView):
         return super().get_queryset()  # type: ignore
 
 
-class DweetCreateView(DweetFormMixin, CreateView):
-    model: Optional[Type[Model]] = Dweet
-    form_class: Optional[Type[BaseForm]] = DweetForm
+class DweetCreateView(DweetFormMixin, ProcessFormView):
+    def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponseRedirect:
+        return HttpResponseRedirect(reverse("dwitter:dashboard"))
 
-    def form_valid(self, form: BaseModelForm) -> HttpResponse:
-        form.instance.user = self.request.user
-        return super().form_valid(form)
-
-    def get_success_url(self):
+    def post(self, request: HttpRequest, *args, **kwargs) -> HttpResponseRedirect:
         """
-        Redirect the user to the page they submitted from
+        Handle POST requests: instantiate a form instance with the passed
+        POST variables and then check if it's valid.
         """
-        return self.request.META.get("HTTP_REFERER")
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
 
 
 class ProfileDetailView(DweetFormMixin, DetailView):
@@ -70,6 +89,9 @@ class ProfileDetailView(DweetFormMixin, DetailView):
         page_number = self.request.GET.get("page", 1)
         context["page_obj"] = paginator.page(page_number)
         context["paginator"] = paginator
+        context["display_follow"] = True
+        if self.request.user.is_authenticated and self.request.user.profile != self.object:
+            context["display_dweet_form"] = False
         return context
 
 
@@ -78,7 +100,7 @@ class ProfileFollowView(SingleObjectMixin, View):
     slug_field: str = "user__username"
     slug_url_kwarg: str = "username"
 
-    def post(self, request: HttpRequest, *args, **kwargs):
+    def post(self, request: HttpRequest, *args, **kwargs) -> HttpResponseRedirect:
         # AnonymousUser cannot follow anyone
         if not request.user.is_authenticated:
             return HttpResponseForbidden()
@@ -98,7 +120,7 @@ class ProfileFollowView(SingleObjectMixin, View):
         return HttpResponseRedirect(reverse("dwitter:profile-detail", kwargs={"username": self.object.user.username}))
 
 
-class ProfileListView(ListView):
+class ProfileListView(DweetFormMixin, ListView):
     # ListView will automagically look for dwitter/templates/dwitter/<model>_list.html for template
     model: Optional[Type[Model]] = Profile
     paginate_by: int = 5
